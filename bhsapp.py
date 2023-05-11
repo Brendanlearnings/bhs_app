291533c2716f837a5332ab4a2d434cd210ee2f0e
Brendanlearnings <brendanlearnings@gmail.com>
2023-05-11 19:11:54 +0200

    test session state

--- bhsapp.py
+++ bhsapp.py
@@ -17,7 +17,7 @@ def details():
     st.write(st.__version__)
     st.title('Class of 2013 Reunion')
     st.write()
-    st.write(st.session_state.name)
+    st.write(st.session_state)
     st.write("""Calling all Matrix Class of 2013 graduates from Paarl Boys High! 
     Get ready to embark on a thrilling journey down memory lane as we celebrate our monumental 10 year reunion. The anticipation is building, and the excitement is contagious! On the 5th of August 2023, our beloved school rugby field will transform into a pulsating hub of nostalgia and camaraderie. 
     Picture yourself surrounded by familiar faces, reliving those glorious moments that shaped our youth. The air will be filled with thunderous cheers, the echoes of our triumphs, and the unbreakable bonds of friendship. This is your chance to reconnect with old teammates, share stories of triumphs and challenges, and reignite the spirit that once burned so bright within us. Let's come together and create an unforgettable experience, where the spirit of the Matrix Class of 2013 shines once again. 
