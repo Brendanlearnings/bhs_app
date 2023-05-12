78370363f82eea2c49107cb603d25c438de71042
Brendanlearnings <brendanlearnings@gmail.com>
2023-05-12 10:36:36 +0200

    updates

--- bhsapp.py
+++ bhsapp.py
@@ -66,8 +66,6 @@ def contact_deets():
 
     if st.button('Submit'):
         st.session_state.name, st.session_state.surname, st.session_state.phone, st.session_state.address = name, surname, phone, address
-        st.write(st.session_state)
-        st.write(f"INSERT INTO BHSAPP.APPDATA.USER_DETAILS (USER_ID,NAME,SURNAME,PHONE,ADDRESS,TMSTMP) VALUES ({st.session_state.user},'{st.session_state.name}','{st.session_state.surname}','{st.session_state.phone}','{st.session_state.address}','{datetime.now()}')")
         run_query(f"INSERT INTO BHSAPP.APPDATA.USER_DETAILS (USER_ID,NAME,SURNAME,PHONE,ADDRESS,TMSTMP) VALUES ({st.session_state.user},'{st.session_state.name}','{st.session_state.surname}','{st.session_state.phone}','{st.session_state.address}','{datetime.now()}')")
         st.write('Successfully captured your data!')
 
@@ -89,6 +87,7 @@ def merch():
     size_select = st.selectbox('Size',sizes)
     if st.button('Submit'):
         st.session_state.size_select = size_select
+        # run_query(f"INSERT INTO BHSAPP.APPDATA.USER_DETAILS (USER_ID,NAME,SURNAME,PHONE,ADDRESS,TMSTMP) VALUES ({st.session_state.user},'{st.session_state.name}','{st.session_state.surname}','{st.session_state.phone}','{st.session_state.address}','{datetime.now()}')")
         st.write('Successfully captured your data!')
 
 
