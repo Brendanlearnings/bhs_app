9e9cd2cf520780edad9f35f89abdbe3f3f788b6f
Brendanlearnings <brendanlearnings@gmail.com>
2023-06-26 23:29:59 +0200

    let see

--- bhsapp.py
+++ bhsapp.py
@@ -98,23 +98,31 @@ def merch():
 def choices():
     choices = ['No','Yes']
     inter_tickets = ['Walk-in','Stand']
+    obj = {
+        'member': None,
+        'ticket': None,
+        'reunion': None
+    }
     if st.session_state.event is not None or len(st.session_state.event) != 0:
         for i in st.session_state.event:
             if 'Friday Big Brag (Stadsaal)' in i:
                 member = st.selectbox('Are you a paid up OBU Member?',choices)
+                obj['member'] = member
             if 'Interschools Rugby' in i:
                 ticket_type = st.selectbox('Interschools Rugby ticket type', inter_tickets)
+                obj['ticket'] = ticket_type
             if '10 Year Reunion Dinner' in i:
                 reunion = st.selectbox('Is your partner attending the reunion dinner?', choices)
+                obj['reunion'] = reunion
     else:
         st.write('No need for any additional questions, thank you!')
 
     if st.button('Submit'):
-        if member:
+        if obj['member'] is not None:
             st.session_state.member = member
-        if ticket_type:
+        if obj['ticket'] is not None:
             st.session_state.ticket_type = ticket_type
-        if reunion:
+        if obj['reunion'] is not None:
             st.session_state.reunion = reunion
         st.write(st.session_state)
         # run_query(f"INSERT INTO BHSAPP.APPDATA.EVENTS (USER_ID, EVENT_, ADDITION, TMSTP) VALUES ({st.session_state.user},'Friday Big Brag (Stadsaal)','{member}','{datetime.now()}')")
